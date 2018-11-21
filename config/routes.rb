5c4fc5199ce1f7b680fbd23b0e9cdba8cd06d4a4
Alice Bouffard <alice.bouffard@gmail.com>
2018-11-21 17:26:25 +0100

    exo 2 solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  resources :restaurants , only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+
+
+  # restaurants
+  # GET "restaurants" index
+
+  # GET "restaurants/new" new
+  # POST "restaurants" create
+
+  # GET "restaurants/:restaurant_id" show
+
+  # reviews
+  # GET "restaurants/:restaurant_id/reviews/new" new
+  # POST "restaurants/:restaurant_id/reviews" create
+
+  # index, new, create, show, edit, update, destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+resto_one = Restaurant.create(name: 'Piccolo Mondo', address: 'Neuilly-sur-Seine', category: 'italian')
+resto_two = Restaurant.create(name: 'Sans Souci', address: 'Pereire', category: 'chinese')
+resto_three = Restaurant.create(name: 'Sushishop', address: 'online', category: 'japanese')
+resto_four = Restaurant.create(name: 'L Entrecote', address: 'Porte Maillot', category: 'french')
+resto_five = Restaurant.create(name: 'Fries and Waffles', address: 'abroad', category: 'belgian')
